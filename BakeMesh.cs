aa08d41f4ea0a4259bff7b5fba00f2a09252dbff
Daniele Pomilio <danipomix@libero.it>
2020-05-29 16:37:49 +0200

    bakemesh

---
+++ BakeMesh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BakeMesh : MonoBehaviour {
+    public SkinnedMeshRenderer SkinnedMR;
+    public MeshFilter meshF;
+    Mesh staticMesh;
+
+	void Start () {
+        staticMesh = new Mesh();
+        meshF.mesh = staticMesh;
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("GetStaticMesh"))
+            getSnapshot();
+    }
+
+    void getSnapshot() {
+        SkinnedMR.BakeMesh(staticMesh);
+    }
+}
